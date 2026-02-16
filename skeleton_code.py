993f3d0ed5fdcb5d83125f1d36eba463a741a547
siddiq222 <skhussainsiddik235@gmail.com>
2026-02-16 11:48:04 +0530

    calculator skeleton code

---
+++ skeleton_code.py
@@ -0,0 +1,12 @@
+class calculator:
+    def __init__(self,x1,x2):
+        self.x1=x1
+        self.x2=x2
+    def add(self):
+        pass
+    def sub(self):
+        pass
+    def mul(self):
+        pass
+    def div(self):
+        pass
